586e06e6853a9c8112c671fa115766794acd0c22
Gia Nguyen <gnguyen1011@gmail.com>
2024-04-24 15:16:07 +0200

    added button with some fluff

--- lit-button.d.ts
+++ lit-button.d.ts
@@ -1,22 +1,14 @@
 import { LitElement } from 'lit';
-/**
- * An example element.
- *
- * @fires count-changed - Indicates when the count changes
- * @slot - This element has a slot
- * @csspart button - The button
- */
 export declare class MyElement extends LitElement {
     static styles: import("lit").CSSResult;
-    name: string;
     count: number;
+    buttonRef: import("lit-html/directives/ref").Ref<HTMLButtonElement>;
+    fireGifSources: string[];
     render(): import("lit-html").TemplateResult<1>;
     private _onClick;
-    /**
-     * Formats a greeting
-     * @param name The name to say "Hello" to
-     */
-    sayHello(name: string): string;
+    private doShake;
+    private getImageSrc;
+    private clampIndex;
 }
 declare global {
     interface HTMLElementTagNameMap {
--- lit-button.js
+++ lit-button.js
@@ -6,51 +6,88 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-/**
- * An example element.
- *
- * @fires count-changed - Indicates when the count changes
- * @slot - This element has a slot
- * @csspart button - The button
- */
+import { ref, createRef } from 'lit/directives/ref.js';
 let MyElement = class MyElement extends LitElement {
     constructor() {
         super(...arguments);
-        this.name = 'World';
         this.count = 0;
+        this.buttonRef = createRef();
+        this.fireGifSources = [
+            "https://gifdb.com/images/high/one-piece-ace-body-on-fire-2u2nfim5jsa9qxze.gif",
+            "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGE2bmdlbzN4bXV0MmpqdDF5MzBkczUzanRkYjRrYng1aGhjZ2llciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pi0A1X7MG5TTq/giphy.gif",
+            "https://media.giphy.com/media/6r4fo21qhu7rW/giphy.gif?cid=790b7611azf0lzx13pc8v3eyt6rngb70hou9r3x3fh0sdimt&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+            "https://media.giphy.com/media/XJMFmuG2zzjCE/giphy.gif?cid=790b76112wb8h2c43la5dtwnba3qo3vcjt032m7em5j8xold&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+            "https://media.giphy.com/media/3XYuxLiRtIu76/giphy.gif?cid=790b76112wb8h2c43la5dtwnba3qo3vcjt032m7em5j8xold&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+            "https://25.media.tumblr.com/ce4b5330ef4272c2d162ff14c55c126e/tumblr_mss2lxtJqd1sg962ho1_500.gif",
+            "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDg3MDluNWRmY2JyNW5ieG1teGVjaHBleXl4ZDg2ajB2MTE4NWpnaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/yr7n0u3qzO9nG/giphy.gif"
+        ];
     }
     render() {
         return html `
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
-    `;
+            <button ${ref(this.buttonRef)} @click=${this._onClick} part="button">
+                <label for="button">🔥 Click Count:</label> ${this.count}
+            </button>
+            <div>
+                ${this.count > 0 ? html `<img src=${this.getImageSrc()} alt="fire" width="150">` : ''}
+            </div>
+        `;
     }
     _onClick() {
         this.count++;
-        this.dispatchEvent(new CustomEvent('count-changed', { detail: this.count }));
+        this.doShake();
+        // event for external listeners
+        this.dispatchEvent(new CustomEvent('onLitClicked'));
     }
-    /**
-     * Formats a greeting
-     * @param name The name to say "Hello" to
-     */
-    sayHello(name) {
-        return `Hello, ${name}`;
+    doShake() {
+        const fireIcon = this.buttonRef.value;
+        const maxShakeDuration = 0.15;
+        let shakeDuration = Math.min(0.067 * this.count, maxShakeDuration);
+        fireIcon.classList.add('dynamic-shake');
+        fireIcon.style.setProperty('--shake-distance', `${this.count}px`);
+        fireIcon.style.setProperty('--shake-duration', `${shakeDuration}s`);
+        setTimeout(() => {
+            fireIcon.classList.remove('dynamic-shake');
+        }, 700);
+    }
+    getImageSrc() {
+        return this.fireGifSources[this.clampIndex(this.count - 1)];
+    }
+    clampIndex(index) {
+        return Math.min(Math.max(index, 0), this.fireGifSources.length - 1);
     }
 };
 MyElement.styles = css `
-    :host {
-      display: block;
-      border: solid 1px gray;
-      padding: 16px;
-      max-width: 800px;
+    button {
+      background-color: #6200ee;
+      color: white;
+      padding: 8px 16px;
+      font-size: 16px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      margin-bottom: 16px;
+    }
+   
+    button:hover { 
+      cursor: pointer;
+    }
+      
+    .dynamic-shake {
+        --shake-distance: 2px;
+        --shake-duration: 0.1s;
+
+      animation-name: shaker;
+      animation-duration: var(--shake-duration);
+      transform-origin: 50% 50%;
+      animation-timing-function: linear;
+    }
+
+    @keyframes shaker {
+      0% { transform: translate(var(--shake-distance), 0); }
+      50% { transform: translate(calc(-1 * var(--shake-distance)), 0); }
+      100% { transform: translate(var(--shake-distance), 0); }
     }
   `;
-__decorate([
-    property()
-], MyElement.prototype, "name", void 0);
 __decorate([
     property({ type: Number })
 ], MyElement.prototype, "count", void 0);
